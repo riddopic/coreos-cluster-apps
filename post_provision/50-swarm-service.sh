cc09246c0b0691de3d5e96b762f90bf31294ab9d
Stefano Harding <riddopic@gmail.com>
2016-06-18 04:31:29 -0700

    use weave overlay

--- post_provision/50-swarm-service.sh
+++ post_provision/50-swarm-service.sh
@@ -4,9 +4,11 @@
 
 . /etc/swarm-node.env
 
+IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4/)
+
 cat <<EOF> /etc/systemd/system/docker.service.d/50-insecure-registry.conf;
 [Service]
-Environment=DOCKER_OPTS='-H=0.0.0.0:2376 -H unix:///var/run/docker.sock --insecure-registry=10.0.0.0/8,registry.docker.local --cluster-advertise $(curl -s http://169.254.169.254/latest/meta-data/local-ipv4/):2376 --cluster-store etcd://127.0.0.1:2379'
+Environment=DOCKER_OPTS='-H=0.0.0.0:2376 -H unix:///var/run/docker.sock --insecure-registry=10.0.0.0/8,registry.docker.local --bip="172.17.43.1/16" --cluster-advertise $IP:2376 --cluster-store etcd://127.0.0.1:2379'
 EOF
 
 systemctl daemon-reload
@@ -16,17 +18,15 @@ docker pull swarm:latest
 
 docker stop swarm-agent
 docker rm swarm-agent
-docker run -d \
-  --name swarm-agent \
-  --net=host \
-  swarm:latest join \
-  --addr=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4/):2376 \
-  etcd://127.0.0.1:2379
+docker run -d --name swarm-agent \
+              --net=host \
+              swarm:latest join \
+              --addr=$IP:2376 \
+              etcd://127.0.0.1:2379
 
 docker stop swarm-manager
 docker rm swarm-manager
-docker run -d \
-  --name swarm-manager \
-  --net=host \
-  swarm:latest manage \
-  etcd://127.0.0.1:2379
+docker run -d --name swarm-manager \
+              --net=host \
+              swarm:latest manage \
+              etcd://127.0.0.1:2379
